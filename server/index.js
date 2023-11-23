2f021b601f853f8396e8d9721e36ba65eb7bc55c
sergbylym <sergbylym@gmail.com>
2023-11-23 18:06:37 +0200

    change .env congit index.js

--- server/index.js
+++ server/index.js
@@ -4,4 +4,20 @@ import dotenv from "dotenv"
 import mongoose from "mongoose";
 
 
+dotenv.config()
+mongoose.connect(process.env.MONGODB_URI).then(() => console.log("Connected to MongoDb") )
+.catch((error) => console.log(error))
 
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+const PORT = process.env.PORT || 5555
+
+app.listen(PORT, (error) => {
+    if(error) {
+        return console.log(error)
+    }
+    console.log(`Server is running  on port ${PORT}`)
+})
