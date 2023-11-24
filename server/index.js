e5a63a24b7ae537e48f167b388195b6a61560636
sergbylym <sergbylym@gmail.com>
2023-11-24 19:58:16 +0200

    rename helpers to utility

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ import  express from "express";
 import cors from "cors"
 import dotenv from "dotenv" 
 import mongoose from "mongoose";
-import { registerValidator } from "./helpers/validators.js";
+import { registerValidator } from "./utility/validators.js";
 import { register } from "./controllers/UserController.js ";
 
 
