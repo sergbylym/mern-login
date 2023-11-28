8f58654b98a434cf1c1ce73f632628c7cf9b7d4f
sergbylym <sergbylym@gmail.com>
2023-11-28 18:40:10 +0200

    create Home page

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -1,9 +1,32 @@
-
+import styles from "../Home/Home.module.scss";
+import { Button, Typography } from "@mui/material";
 
 const Home = () => {
+  const isAuth = false;
+  const userEmail = "sergbylym@gmail.com";
   return (
-    <div> Home</div>
-  )
-}
+    <div className={styles.container}>
+      {isAuth && (
+        <>
+          <Typography variant="h3">You are logged in !</Typography>
+          <Typography variant="h3">User Email : {userEmail}</Typography>
+        </>
+      )}
+
+      <div className={styles.buttonsContainer}>
+        {isAuth ? (
+          <Button variant="contained" onClick={() => console.log("Logout")}>
+            Logout
+          </Button>
+        ) : (
+          <>
+            <Button variant="contained" onClick={() => console.log("Log In")}> Log in</Button>
+            <Button variant="contained" onClick={() => console.log("Register")}> Register</Button>
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
 
 export default Home;
