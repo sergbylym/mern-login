bce5d62d2456dc8e554afc2abec964095316fe1e
sergbylym <sergbylym@gmail.com>
2023-11-29 13:37:42 +0200

    create AuthForm

--- client/src/components/AuthForm/index.jsx
+++ client/src/components/AuthForm/index.jsx
@@ -1,9 +1,39 @@
-
-
-const AuthForm = () => {
+import { Button, TextField, Typography } from "@mui/material";
+import styles from "./AuthForm.module.scss";
+import { useForm } from "react-hook-form";
+const AuthForm = ({ formType, handleModalClose }) => {
+  const isRegisterFormType = formType === "register";
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm({ defaultValues: { email: "", password: "" }, mode: "onChange" });
+  const onSubmit = (data) => console.log(data);
   return (
-    <div>AuthForm</div>
-  )
-}
+    <>
+      <Typography variant="h2" className={styles.title}>
+        {isRegisterFormType ? "Register" : "Login"}
+      </Typography>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <TextField
+          label="Email"
+          type="email"
+          {...register("email", { required: "Email field is required" })}
+          errors={Boolean(errors?.email?.message)}
+          helperText={errors.email ? "Email field is required" : ""}
+        />
+        <TextField   label="Password"
+          type="password"
+          {...register("password", { required: "Password field is required", minLength: 6, })}
+          errors={Boolean(errors?.password?.message)}
+          helperText={errors.password? "Password field is required" : ""}/>
 
-export default AuthForm
+        <Button disabled={!isValid} variant="contain" type="submit">
+          Submit
+        </Button>
+      </form>
+    </>
+  );
+};
+
+export default AuthForm;
--- client/src/components/Modals/index.jsx
+++ client/src/components/Modals/index.jsx
@@ -1,10 +1,13 @@
 import { Box, Modal } from "@mui/material";
+import AuthForm from "../AuthForm";
 import styles from "./Modals.module.scss"
 const Modals = ({type, handleModalClose}) => {
   return (
     <>
       <Modal open={type === "login"} onClose={handleModalClose}>
-      <Box className={styles.formContainer}> Login Modal</Box> 
+      <Box className={styles.formContainer}>
+      <AuthForm formType={type} handleModalClose={handleModalClose}/>  
+      </Box> 
       </Modal>
       <Modal open={type === "register"} onClose={handleModalClose}>
         <Box className={styles.formContainer}>Register Modal</Box>
