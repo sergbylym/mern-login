12fc53fda95e4a62feaab457bd922f32d1f75b28
sergbylym <sergbylym@gmail.com>
2023-11-29 14:17:40 +0200

    create .env for client + finished authSlice

--- client/src/redux/slices/auth.js
+++ client/src/redux/slices/auth.js
@@ -1,4 +1,7 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "../../axiosAPI.js";
+
+
 const registration = createAsyncThunk("auth-registration", async (params) => {
     const {data} = await axios.post('/register', params)
     return data;
@@ -19,6 +22,21 @@ const authSlice = createSlice({
     },
 
     extraReducers: {
-        []
+        [registration.pending]: (state) => {
+            state.userEmail = null
+            state.status = "loading"
+        },
+        [registration.fulfilled]: (state,action) => {
+            state.userEmail = action.payload.email
+            state.status = "loaded"
+        },
+        [registration.rejected]: (state) => {
+            state.userEmail = null
+            state.status = "error"
+        },
     }
 })
+
+export const isAuthSelector = (state) => Boolean(state.auth.userEmail)
+export const authReducer = authSlice.reducer
+export const {logout} = authSlice.actions
