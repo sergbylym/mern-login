d41053ebb2b96543c33e38fa19a55cf990db28d6
sergbylym <sergbylym@gmail.com>
2023-12-01 18:28:09 +0200

    create fetchUserData for auth.js

--- client/src/redux/slices/auth.js
+++ client/src/redux/slices/auth.js
@@ -1,61 +1,85 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "../../axiosAPI.js";
 
-
- export const registration = createAsyncThunk("auth-registration", async (params) => {
-    const {data} = await axios.post('/register', params)
+export const registration = createAsyncThunk(
+  "auth-registration",
+  async (params) => {
+    const { data } = await axios.post("/register", params);
     return data;
-})
+  }
+);
 
-export const loginization = createAsyncThunk("auth-registration", async (params) => {
-    const {data} = await axios.post('/login', params)
+export const loginization = createAsyncThunk(
+  "auth-registration",
+  async (params) => {
+    const { data } = await axios.post("/login", params);
     return data;
-})
+  }
+);
+
+export const fetchUserData = createAsyncThunk(
+  "fetchUserData",
+  async () => {
+    const { data } = await axios.get("/user");
+    return data;
+  }
+);
 
 const initialState = {
   userEmail: null,
- status: "loading",
+  status: "loading",
 };
 
-
 const authSlice = createSlice({
-    name: "auth",
-    initialState,
-    reducers: {
-        logout: (state) => {
-            state.userEmail = null
-            window.localStorage.removeItem("token")
-        },
+  name: "auth",
+  initialState,
+  reducers: {
+    logout: (state) => {
+      state.userEmail = null;
+      window.localStorage.removeItem("token");
     },
+  },
 
-    extraReducers: {
-        [registration.pending]: (state) => {
-            state.userEmail = null
-            state.status = "loading"
-        },
-        [registration.fulfilled]: (state,action) => {
-            state.userEmail = action.payload.email
-            state.status = "loaded"
-        },
-        [registration.rejected]: (state) => {
-            state.userEmail = null
-            state.status = "error"
-        },
-        [loginization.pending]: (state) => {
-            state.userEmail = null
-            state.status = "loading"
-        },
-        [loginization.fulfilled]: (state,action) => {
-            state.userEmail = action.payload.email
-            state.status = "loaded"
-        },
-        [loginization.rejected]: (state) => {
-            state.userEmail = null
-            state.status = "error"
-        },
-    }
-})
+  extraReducers: {
+    [registration.pending]: (state) => {
+      state.userEmail = null;
+      state.status = "loading";
+    },
+    [registration.fulfilled]: (state, action) => {
+      state.userEmail = action.payload.email;
+      state.status = "loaded";
+    },
+    [registration.rejected]: (state) => {
+      state.userEmail = null;
+      state.status = "error";
+    },
+    [loginization.pending]: (state) => {
+      state.userEmail = null;
+      state.status = "loading";
+    },
+    [loginization.fulfilled]: (state, action) => {
+      state.userEmail = action.payload.email;
+      state.status = "loaded";
+    },
+    [loginization.rejected]: (state) => {
+      state.userEmail = null;
+      state.status = "error";
+    },
+    [fetchUserData.pending]: (state) => {
+        state.userEmail = null;
+        state.status = "loading";
+      },
+      [fetchUserData.fulfilled]: (state, action) => {
+        state.userEmail = action.payload.email;
+        state.status = "loaded";
+      },
+      [fetchUserData.rejected]: (state) => {
+        state.userEmail = null;
+        state.status = "error";
+      },
+  },
+});
 
-export const isAuthSelector = (state) => Boolean(state.auth.userEmail)
-export const authReducer = authSlice.reducer
-export const {logout} = authSlice.actions
+export const isAuthSelector = (state) => Boolean(state.auth.userEmail);
+export const authReducer = authSlice.reducer;
+export const { logout } = authSlice.actions;
